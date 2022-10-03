b9879125c25794360cf8980a6935438a403e5a21
Arik Smith <ams382@case.edu>
2022-10-03 18:10:19 -0400

    Reverts test setup file

--- plugins/backend/src/setupTests.ts
+++ plugins/backend/src/setupTests.ts
@@ -2,8 +2,4 @@
  * © 2022 Thoughtworks, Inc.
  */
 
-// Temporary fix to weird text encoder issue on line 5 of router.test.ts file
-import { TextEncoder, TextDecoder } from 'util'
-
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+export {}
