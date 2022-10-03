2f840afd7dca86e2594ef9a9cec0c9b39153fdf8
Arik Smith <ams382@case.edu>
2022-10-03 17:02:43 -0400

    Fixes failing test due to TextEncoder error

--- plugins/backend/src/setupTests.ts
+++ plugins/backend/src/setupTests.ts
@@ -2,4 +2,8 @@
  * © 2022 Thoughtworks, Inc.
  */
 
-export {};
+// Temporary fix to weird text encoder issue on line 5 of router.test.ts file
+import { TextEncoder, TextDecoder } from 'util'
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
